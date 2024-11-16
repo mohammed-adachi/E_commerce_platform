a7b296fc9b2b14dd9432a7277ed7dfd23e3cf293
Mohamed <mohame@gmail.com>
2024-11-16 17:28:35 +0100

    footer

---
+++ includes/footer.php
@@ -0,0 +1,18 @@
+
+
+
+
+
+        <footer class="foot">
+           <div class="container">
+               <center>
+                   <p>Nous somme ici pour vous <small>&copy;</small>  Prodigy InfoTech 2024</p>
+               </center>
+           </div>
+           
+           
+       </footer>   
+        
+        
+        
+       
